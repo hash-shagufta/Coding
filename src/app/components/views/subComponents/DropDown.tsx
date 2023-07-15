73a65ffde39904514b63861904e54f02b8257715
Shagufta <shafishagufta1@gmail.com>
2023-07-15 07:32:31 -0700

    resolve deployment issue

--- src/app/components/views/subComponents/DropDown.tsx
+++ src/app/components/views/subComponents/DropDown.tsx
@@ -6,7 +6,7 @@ const DropDown : FC <{ item:NavbarItemType }> = ({ item }) => {
   return (
    <ul>
     {item.dropDownData?.map((item:NavbarItemType, index:number) =>(
-<li className='hover:ml-2 group-hover:duration-300 -translate-y-5'>
+<li key={index} className='hover:ml-2 group-hover:duration-300 -translate-y-5'>
     <Link  href={item.href}>{item.label}</Link>
 </li>
 
--- src/app/components/views/subComponents/Expand.tsx
+++ src/app/components/views/subComponents/Expand.tsx
@@ -23,12 +23,12 @@ const Expand: FC<{item:NavbarItemType }> = ({item}) => {
   return (
         <li className={`${isExpended? "h-48" : "h-12"}   list-none`}>
           <div onClick={handleExpand} className="flex hover:bg-purple-500 py-2 px-3  rounded-md duration-300 items-center justify-between">
-          <Link href={item.href}>{item.label}</Link>
+          <Link  href={item.href}>{item.label}</Link>
           {item.isDropDown ?  <HiOutlineChevronDown className="mt-1 -rotate-180 group-hover:rotate-0 duration-300" size={16}/>: ""} 
           </div>
           <div className="flex flex-col space-y-1 mt-2">
            {isTimeOut && item.dropDownData?.map((subItem: NavbarItemType,index:number)=> (
-           <Link className="hover:bg-gray-200 rounded-md py-1 px-5 duration-300" href={subItem.href}>
+           <Link key={index} className="hover:bg-gray-200 rounded-md py-1 px-5 duration-300" href={subItem.href}>
            {subItem.label}
            </Link>
         
